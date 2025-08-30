980897bc5d346fbf46b7389746aac1f37cb6e558
zkils <zkils8302@gmail.com>
2025-08-30 11:56:23 +0900

    public_path 수정

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/CatClassroomGame/'   // 저장소 이름
-    : '/'
+  publicPath: '/'
 })
